befac6726beeeb786b469477dd70d2ab9c7054db
Sergi <meuxilus@gmail.com>
2019-01-31 14:46:32 +0100

    init

---
+++ src/main/java/net/portic/fsm/doc/fsmdoc/FsmDocApplication.java
@@ -0,0 +1,14 @@
+package net.portic.fsm.doc.fsmdoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsmDocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsmDocApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/net/portic/fsm/doc/fsmdoc/FsmDocApplicationTests.java
@@ -0,0 +1,17 @@
+package net.portic.fsm.doc.fsmdoc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FsmDocApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
